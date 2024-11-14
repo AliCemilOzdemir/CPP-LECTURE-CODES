e457f2d64ddd1fc47b5ff50ed49d10264b175450
Ali Cemil Özdemir <152206260+AliCemilOzdemir@users.noreply.github.com>
2024-11-14 12:11:20 +0300

    Add files via upload

---
+++ WEEK2/example1.cpp
@@ -0,0 +1,14 @@
+// github.com/AliCemilOzdemir
+// Second Week Slide Page 4
+
+#include <iostream> // include library to output data to the screen
+using namespace std; // use standard namespace
+
+int main() // function main begins program execution
+{
+    // A simple program
+    cout << "Welcome to C++!" << endl; // display message
+
+return 0; // indicate that program ended successfully
+
+} // end function main

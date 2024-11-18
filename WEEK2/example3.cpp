f9a22c6fa073a15e27b69197a08818821a5221c2
Ali Cemil Özdemir <152206260+AliCemilOzdemir@users.noreply.github.com>
2024-11-18 22:38:18 +0300

    Add files via upload

---
+++ WEEK2/example3.cpp
@@ -0,0 +1,14 @@
+// github.com/AliCemilOzdemir
+// Second Week Slide Page 22
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Example : Escape Sequence 
+    
+    cout << "This\nis\na\ntest\n\nShe said, \"Hello!\" ";
+
+return 0;
+}

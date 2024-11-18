e557e557e493a9d06d184b8d68809b71f4f39c66
Ali Cemil Özdemir <152206260+AliCemilOzdemir@users.noreply.github.com>
2024-11-18 22:37:57 +0300

    Add files via upload

---
+++ WEEK2/example2.cpp
@@ -0,0 +1,16 @@
+// github.com/AliCemilOzdemir
+// Second Week Slide Page 20
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // INPUT/OUTPUT EXAMPLE
+    int i;
+    cout << "Please print a number: ";
+    cin >> i;
+    cout << "you entered: " << i << endl;
+
+return 0;
+}

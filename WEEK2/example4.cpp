da881ee1a9d71b9a682b0e567c446c3efda7c872
Ali Cemil Özdemir <152206260+AliCemilOzdemir@users.noreply.github.com>
2024-11-19 20:35:03 +0300

    Add files via upload

---
+++ WEEK2/example4.cpp
@@ -0,0 +1,21 @@
+// github.com/AliCemilOzdemir
+// Second Week Slide Page 24
+
+#include <iostream>
+using namespace std;
+int main ()
+{
+    // Example: Assignment Operators
+
+    int a, b; // a:?, b:?
+
+    a = 10; // a:10, b:?
+    b = 4; // a:10, b:4
+    cout << "a: " << a << ", b: " << b << endl;
+
+    a = b; // a:4, b:4
+    b = b/2; // a:4, b:2
+    cout << "a: " << a << ", b: " << b << endl;
+
+return 0;
+}

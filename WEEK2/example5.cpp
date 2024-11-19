7451dd1bf0b0506bf3a7837d6311dd68a2ab2b62
Ali Cemil Özdemir <152206260+AliCemilOzdemir@users.noreply.github.com>
2024-11-19 22:14:47 +0300

    Add files via upload

---
+++ WEEK2/example5.cpp
@@ -0,0 +1,18 @@
+// github.com/AliCemilOzdemir
+// Second Week Slide Page 28
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Example: Arithmetic Assignment Operators
+    
+    int number1 = 15;
+    cout <<"number1 = " << number1 << endl;
+    
+    number1 +=5;
+    cout <<"number1 = " << number1 << endl;
+
+return 0;
+}

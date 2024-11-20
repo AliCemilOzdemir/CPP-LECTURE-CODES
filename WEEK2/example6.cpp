fd4fb56c92c45a884a1a9037271ad39068d4bd68
Ali Cemil Özdemir <152206260+AliCemilOzdemir@users.noreply.github.com>
2024-11-20 13:31:42 +0300

    Add files via upload

---
+++ WEEK2/example6.cpp
@@ -0,0 +1,19 @@
+// github.com/AliCemilOzdemir
+// Second Week Slide Page 30
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Example: Increment Operators
+
+    int number1 = 5;
+    
+    cout << "number1 is " << number1 << endl;
+    cout << "number1++ is " << number1++ << endl;
+    cout << "++number1 is " << ++number1 << endl;
+    cout << "finally, number1 is now " << number1 << endl;
+
+return 0;
+}

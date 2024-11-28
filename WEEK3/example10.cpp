8ab727edadf105eec0b43c35ac3292d3ddd61537
Ali Cemil Özdemir <152206260+AliCemilOzdemir@users.noreply.github.com>
2024-11-29 00:42:50 +0300

    Add files via upload

---
+++ WEEK3/example10.cpp
@@ -0,0 +1,14 @@
+// github.com/AliCemilOzdemir
+// Third Week Slide Page 27
+#include<iostream>
+using namespace std;
+int main()
+{
+    //  Another Enum Example
+    enum TV{CNN=25,MTV=15,ATV=22,FOX=30,NBC=32};
+    
+    cout<<"List of TVstations: " <<endl;
+    cout<<"FOX:"<<FOX<<endl;
+    cout<<"CNN:"<<CNN<<endl;
+    cout<<"NBC:"<<NBC<<endl;
+}

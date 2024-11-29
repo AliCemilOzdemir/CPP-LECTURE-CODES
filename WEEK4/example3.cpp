831b3d31629472c2a4941fcdae0aa7aa6a102c8b
Ali Cemil Özdemir <152206260+AliCemilOzdemir@users.noreply.github.com>
2024-11-29 09:53:04 +0300

    Add files via upload

---
+++ WEEK4/example3.cpp
@@ -0,0 +1,16 @@
+// github.com/AliCemilOzdemir
+// Fourth Week Slide Page 6
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    // InfiniteLoopExample-2
+    int num,start =0;
+    cin>>num;
+    while ( start !=num)
+    {
+        start +=2;
+    }
+    return 0;
+}

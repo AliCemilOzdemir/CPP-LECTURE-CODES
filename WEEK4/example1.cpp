56bebfdef540eb8d680fd0f3e58c1b1a2abc717c
Ali Cemil Özdemir <152206260+AliCemilOzdemir@users.noreply.github.com>
2024-11-29 09:51:45 +0300

    Add files via upload

---
+++ WEEK4/example1.cpp
@@ -0,0 +1,15 @@
+// github.com/AliCemilOzdemir
+// Fourth Week Slide Page 4
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i =1;
+    while (i <=10)
+    {
+        cout<<i<<endl;
+        i = i+1;
+    }
+    return 0;
+}

6c5a09206628994448906081281fcac5babeca8f
Ali Cemil Özdemir <152206260+AliCemilOzdemir@users.noreply.github.com>
2024-12-05 22:09:59 +0300

    Add files via upload

---
+++ WEEK4/example14.cpp
@@ -0,0 +1,17 @@
+// github.com/AliCemilOzdemir
+// Fourth Week Slide Page 21
+
+#include<iostream>
+using namespace std;
+int main()
+{
+// Example: Translate while to for Loop
+    int sum = 0;
+    for(int i = 1; i<=10; i+=2)
+    {
+        sum += i;
+    }
+    // cout<<sum;
+
+    return 0;
+}

f0b7fb97994197c16b7672fb589737244fd6d28d
Ali Cemil Özdemir <152206260+AliCemilOzdemir@users.noreply.github.com>
2024-12-05 22:10:44 +0300

    Add files via upload

---
+++ WEEK5/example1.cpp
@@ -0,0 +1,23 @@
+// github.com/AliCemilOzdemir
+// Fifth Week Slide Page 3
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{ 
+    int a=1, b=1;
+    while(a<=5)
+    {
+        b=1;
+        while(b<=5)
+        {
+            cout<<b;
+            b++;
+        }
+        cout<<"\n"; // orcout<<endl;
+        a++;
+    }
+
+    return 0;
+} 

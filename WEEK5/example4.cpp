59db687dd1915f12f8d6c2dd4fb63ccf2555b634
Ali Cemil Özdemir <152206260+AliCemilOzdemir@users.noreply.github.com>
2024-12-14 21:19:47 +0300

    Add files via upload

---
+++ WEEK5/example4.cpp
@@ -0,0 +1,20 @@
+// github.com/AliCemilOzdemir
+// Fifth Week Slide Page 8
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{ 
+    // Write a program that prints half a pyramid using letters
+    for(int i = 1; i<=5; i++)
+    {
+        for(int j = 1; j<=i; j++)
+        {
+            cout<<i;
+        }
+        cout<<endl;
+    }
+
+    return 0;
+} 

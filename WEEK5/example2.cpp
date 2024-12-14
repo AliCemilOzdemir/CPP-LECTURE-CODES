f5a9831766d9abe17d7832b17b0dd11a210f6d96
Ali Cemil Özdemir <152206260+AliCemilOzdemir@users.noreply.github.com>
2024-12-14 21:19:16 +0300

    Add files via upload

---
+++ WEEK5/example2.cpp
@@ -0,0 +1,18 @@
+// github.com/AliCemilOzdemir
+// Fifth Week Slide Page 6
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{ 
+    for ( int outer = 0; outer <= 3; outer++)
+    {
+        for ( int inner = 0; inner <= 2; inner++)
+        {
+            cout << outer << " " << inner << endl;
+        }
+    }
+
+    return 0;
+} 

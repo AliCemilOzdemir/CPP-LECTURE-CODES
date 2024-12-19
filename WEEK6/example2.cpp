b024baad6032ff3e2674a64235ac9d205769fc6c
Ali Cemil Özdemir <152206260+AliCemilOzdemir@users.noreply.github.com>
2024-12-19 12:00:22 +0300

    Add files via upload

---
+++ WEEK6/example2.cpp
@@ -0,0 +1,24 @@
+// github.com/AliCemilOzdemir
+// Sixth Week Slide Page 9
+
+#include <iostream>
+using namespace std;
+
+void Head()
+{
+    cout << " | | | | | | | | | | " << endl;
+    cout << " |                 | " << endl;
+    cout << " |      o   o      | " << endl;
+    cout << " |                 | " << endl;
+    cout << " |        |        | " << endl;
+    cout << " |       ---       | " << endl;
+    cout << " |                 | " << endl;
+    cout << " |_________________| " << endl;
+}
+
+int main()
+{
+    // Example: Drawing a Head with ASCII Art
+    Head();
+    return 0;
+}
